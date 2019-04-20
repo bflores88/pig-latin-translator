2aa2b4ad44b0dd04cac6e612b01c97c644d3bd0e
kmietotwy <kmietotwy@gmail.com>
2019-04-19 18:10:02 -1000

    edited test and fixed pig latin to english

--- test/test.js
+++ test/test.js
@@ -1,36 +1,41 @@
 'use strict';
 
-const pigLatinTranslator = require('../app.js');
+const pigLatinTranslator = require('../js/app.js');
 const englishToPigLatin = pigLatinTranslator.englishToPigLatin;
 const pigLatinToEnglish = pigLatinTranslator.pigLatinToEnglish;
 const chai = require('chai');
 const should = chai.should();
 
-describe ('pig-latin-translator', function (){
+describe ('english to pig latin', function (){
 
   it('should translate words starting with consonates', function() {
-    let translatedWord = englishToPigLatin('cat');
-    translatedWord.should.be.equal('at-cay');
+    englishToPigLatin('cat').should.equal('at-cay');
   });
 
   it('should translate words starting with vowels', function() {
-    let translatedWord = englishToPigLatin('apples');
-    translatedWord.should.be.equal('apples-ay');
+    englishToPigLatin('apples').should.equal('apples-ay');
   });
 
   it('should translate sentences', function() {
-    let translatedWord = englishToPigLatin('pikachu, i choose you!');
-    translatedWord.should.be.equal('ikachu-pay, i-ay oose-chay ou-yay!');
+    englishToPigLatin('pikachu, i choose you!').should.equal('ikachu-pay, i-ay oose-chay ou-yay!');
   });
+
+});
+
+describe ('pig latin to english', function (){
 
   it('should translate words starting with consonates', function() {
     let translatedWord = pigLatinToEnglish('eow-may');
     translatedWord.should.be.equal('meow');
   });
-
+  
   it('should translate words starting with vowels', function() {
     let translatedWord = pigLatinToEnglish('igloo-ay');
     translatedWord.should.be.equal('igloo');
+  });
+
+  it('should translate phrases', function (){
+    pigLatinToEnglish('op-stay, in-ay e-thay ame-nay of-ay ove-lay!').should.equal('stop, in the name of love!');
   });
 
 });
